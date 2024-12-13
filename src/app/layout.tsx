7bec3a69b555c852c6cf0b6d85283ef572a7bece
Phillip Konstantinov <Phillipk0113@gmail.com>
2024-12-12 17:23:02 -0700

    I added some things to the sidebar to start getting a structure for what will be shown on the website. I also played a bit more with the CSS for the Image and Bio section to make it behave better when switching between different screen sizes.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,14 +3,30 @@ import "./globals.css";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupAction,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarInput,
+  SidebarInset,
   SidebarMenu,
-  SidebarMenuItem,
+  SidebarMenuAction,
+  SidebarMenuBadge,
   SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSkeleton,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
   SidebarProvider,
-  SidebarTrigger
+  SidebarRail,
+  SidebarSeparator,
+  SidebarTrigger,
 } from "@/components/ui/sidebar"
 import { lusitana } from '@/app/ui/fonts';
+import { LibraryBig, Github, FileChartColumnIncreasing, FileCode } from 'lucide-react';
 
 
 
@@ -26,11 +42,44 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className= {`${lusitana.className}`}>
+      <body className= {`${lusitana.className} bg-stone-100`}>
         <SidebarProvider>
           {/* Sidebar component */}
-          <Sidebar collapsible="icon">
-            
+          <Sidebar collapsible="offcanvas">
+            <SidebarHeader></SidebarHeader>
+            <SidebarContent>
+              <SidebarGroup>
+                <SidebarGroupLabel>
+                  <LibraryBig/>
+                  <h2 className="pt-1">Book Blog</h2>
+                </SidebarGroupLabel>
+                <SidebarGroupContent>Here is the content</SidebarGroupContent>
+              </SidebarGroup>
+              <SidebarGroup>
+                <SidebarGroupLabel>
+                  <Github/>
+                  <h2 className="pt-1">GitHub Projects</h2>
+                </SidebarGroupLabel>
+                <SidebarGroupContent>Here is the content</SidebarGroupContent>
+              </SidebarGroup>
+              <SidebarGroup>
+                <SidebarGroupLabel>
+                  <FileChartColumnIncreasing/>
+                  <h2 className="pt-1">Financial Analysis Agent</h2>
+                </SidebarGroupLabel>
+                <SidebarGroupContent>Here is the content</SidebarGroupContent>
+              </SidebarGroup>
+              <SidebarGroup>
+                <SidebarGroupLabel className="align-middle">
+                  <FileCode/>
+                  <h2 className="pt-1">Websites</h2>
+                </SidebarGroupLabel>
+                <SidebarGroupContent>
+                  <a href="https://meridianexecutivelimo.com">Meridian Exec Limousines</a>
+                </SidebarGroupContent>
+              </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter></SidebarFooter>
           </Sidebar>
           <SidebarTrigger />
           {/* Main content area */}
